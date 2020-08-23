c8f4007716e3bf46159ad15cb5b5a85b1df832cf
zartre <paulpreuss@gmail.com>
2020-08-23 22:43:36 +0200

    Intents und onActivityResult()

--- app/src/main/java/com/paul/twoactivities/MainActivity.java
+++ app/src/main/java/com/paul/twoactivities/MainActivity.java
@@ -6,20 +6,46 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     final String LOG_TAG = MainActivity.class.getSimpleName();
+    public final static String EXTRA_MESSAGE = "com.paul.twoactivities.extra.MESSAGE"; //key für extra
+    private EditText mMessageEditText;
+    public final static int TEXT_REQUEST = 1;
+    private TextView replyText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMessageEditText = findViewById(R.id.editText_main);
+        replyText = findViewById(R.id.textView_backmessage);
     }
+
 
     public void openSecondActv(View view) {
         Log.i(LOG_TAG, "Button clicked!");
         Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
+        String message = mMessageEditText.getText().toString(); //value (string) für extra
+        Log.i(LOG_TAG, message);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivityForResult(intent, TEXT_REQUEST);
     }
+
+    @Override
+    public void onActivityResult(int requestCode,
+                                 int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply = data.getStringExtra(SecondActivity.SENDBACKMESSAGE_KEY);
+                replyText.setText(reply);
+            }
+        }
+
+    }
+
 }
--- app/src/main/java/com/paul/twoactivities/SecondActivity.java
+++ app/src/main/java/com/paul/twoactivities/SecondActivity.java
@@ -2,13 +2,34 @@ package com.paul.twoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    public final static String SENDBACKMESSAGE_KEY = "com.paul.twoactivities.extra.MESSAGEBACK"; //key für extra
+    private EditText replyEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        replyEditText = findViewById(R.id.text_backmessage);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE); //der key aus main!
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
     }
+
+  public void returnMessage(View view) {
+        Intent replyIntent = new Intent();
+        String replyBack = replyEditText.getText().toString();
+        replyIntent.putExtra(SENDBACKMESSAGE_KEY, replyBack);
+        setResult(RESULT_OK,replyIntent);
+        finish();
+    }
+
 }
